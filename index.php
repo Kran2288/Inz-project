52e739186cb00815aad6dbe2aeffd1201be1cadb
Kran2288 <ashirov.ayrat@gmail.com>
2021-03-09 22:27:55 +0300

    Начинаем

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/style/style.css">
+    <title>Inz-project</title>
+</head>
+<body>
+<div class="row">
+  <div class="col-sm-4">.col-sm-4</div>
+  <div class="col-sm-8">.col-sm-8</div>
+</div>
+</body>
+</html>
