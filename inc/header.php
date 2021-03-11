6e0884eeaa60d31cbc3b06d37b8882cfb17256d9
Kran2288 <ashirov.ayrat@gmail.com>
2021-03-11 18:01:00 +0300

    Сделал header

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<header>
+    <div class="header_width">
+    <nav class = 'header_nav'>
+        <ul class=' row header__list'>
+            <div class="col-md"><li class='header__item'><a href="" class="header_a">Главная.</a></li></div>
+            <div class="col-md"><li class='header__item'><a href="" class="header_a">Новости</a></li></div>
+            <div class="col-md"><li class='header__item'><a href="" class="header_a">О нас</a></li></div>
+            <div class="col-md"><li class='header__item'><a href="" class="header_a">Процесс подготовки</a></li></div>
+            <div class="col-md"><li class='header__item'><a href="" class="header_a">Фотоальбом</a></li></div>
+            <div class="col-md"><li class='header__item'><a href="" class="header_a">Админка</a></li></div>
+        </ul>
+    </nav>
+    <div>
+        <h1 class='header__title '>News from<br> Russia</h1>
+    </div>
+    <div>
+        <h2 class='header__title-text '>Самые интересные новости<br> связанные с россией <br>и не только</h2>
+    </div>
+    <a href="page/new.html" class="btn"><p class="btn-p">Переход по ссылке</p></a>
+    </div>
+
+</header>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    require_once 'include/db.php';
+  ?> 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,34 +8,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="/style/style.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
     <title>Inz-project</title>
 </head>
 <body>
-<?php
-    require_once 'include/db.php';
-  ?> 
-<header>
+  <? include "inc/header.php" ?>
+
+<!-- <header>
   <div class="row header">
     <div class="col-sm-3">.col-sm-4</div>
     <div class="col-sm-8">.col-sm-8</div>
   </div>
-</header>
-<table>
+</header> -->
+
+
+<!-- <table>
   <tr>
     <th>ID</th>
     <th>Title</th>
     <th>Description</th>
   </tr>
-  <?php
-    $news_name = mysqli_query($mysql, query:"")
-  ?>
+  
   <tr>
     <th>1</th>
     <th>News-1</th>
     <th>Lorem ipsum dolor sit amet.</th>
   </tr>
  
-</table>
+</table> -->
 
  
 </body>
